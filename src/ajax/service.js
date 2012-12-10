3ce7143536de5de1ab182b67be3caf218eda8885
Mikael Karon <mikael@karon.se>
2012-12-10 12:33:00 -0600

    moved ajax service from core

---
+++ src/ajax/service.js
@@ -0,0 +1,25 @@
+/*!
+ * TroopJS ajax/service module
+ * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
+ * Released under the MIT license.
+ */
+/*global define:false */
+define([ "troopjs-core/component/service", "jquery", "troopjs-utils/merge" ], function AjaxModule(Service, $, merge) {
+	/*jshint strict:false */
+
+	var TRACE = "trace";
+
+	return Service.extend({
+		displayName : "browser/ajax/service",
+
+		"hub/ajax" : function ajax(topic, settings) {
+			// Request
+			return $.ajax(merge.call({
+				"headers": {
+					"x-request-id": new Date().getTime(),
+					"x-components": topic[TRACE] instanceof Function ? topic[TRACE]() : topic
+				}
+			}, settings));
+		}
+	});
+});
