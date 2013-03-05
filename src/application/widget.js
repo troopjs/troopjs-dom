79b5df76bdf8fd5618e31a72e4353096316d2490
Mikael Karon <mikael@karon.se>
2013-03-05 19:01:48 +0800

    Refactor application widget to not use first arg. see troopjs/troopjs-core#94

--- src/application/widget.js
+++ src/application/widget.js
@@ -8,21 +8,32 @@ define([ "module", "../component/widget", "when", "troopjs-core/registry/service
 
 	var ARRAY_PROTO = Array.prototype;
 	var ARRAY_SLICE = ARRAY_PROTO.slice;
+	var ARRAY_PUSH = ARRAY_PROTO.push;
 	var REGISTRY = "registry";
 
-	function forward(signal) {
+	/**
+	 * Forwards _signal to components
+	 * @private
+	 * @param {String} _signal Signal to forward
+	 * @param {Array} _args Signal arguments
+	 * @returns {Function}
+	 */
+	function forward(_signal, _args) {
 		var self = this;
-		var args = ARRAY_SLICE.call(arguments);
-		var services = self[REGISTRY].get();
-		var service;
+		var signal = self.signal;
+		var args = [ _signal ];
+		var components = self[REGISTRY].get();
 		var index = 0;
 
-		function next() {
-			// Return a chained promise of next signal, or a promise resolved with signal
-			return (service = services[index++])
-				? when(service.signal.apply(service, args), next)
-				: when.resolve(signal);
-		}
+		ARRAY_PUSH.apply(args, _args);
+
+		var next = function () {
+			var component;
+
+			return (component = components[index++])
+				? when(signal.apply(component, args), next)
+				: when.resolve(_args);
+		};
 
 		return next();
 	}
@@ -31,33 +42,40 @@ define([ "module", "../component/widget", "when", "troopjs-core/registry/service
 		// Create registry
 		var registry = this[REGISTRY] = RegistryService();
 
-		// Slice and iterate children
-		ARRAY_SLICE.call(arguments, 2).forEach(function (service) {
-			// Register service
-			registry.add(service);
+		// Slice and iterate arguments
+		ARRAY_SLICE.call(arguments, 2).forEach(function (component) {
+			// Register component
+			registry.add(component);
 		});
 	}, {
 		"displayName" : "browser/application/widget",
 
-		"sig/initialize" : forward,
-		"sig/start" : function start() {
+		"sig/initialize" : function onInitialize() {
+			return forward.call(this, "initialize", arguments);
+		},
+
+		"sig/start" : function onStart() {
 			var self = this;
-			var _weave = self.weave;
+			var weave = self.weave;
 			var args = arguments;
 
-			return forward.apply(self, args).then(function started() {
-				return _weave.apply(self, ARRAY_SLICE.call(args, 1));
+			return forward.call(self, "start", args).then(function started() {
+				return weave.apply(self, args);
 			});
 		},
-		"sig/stop" : function stop() {
+
+		"sig/stop" : function onStop() {
 			var self = this;
-			var _unweave = self.unweave;
+			var unweave = self.unweave;
 			var args = arguments;
 
-			return _unweave.apply(self, ARRAY_SLICE.call(args, 1)).then(function stopped() {
-				return forward.apply(self, args);
+			return unweave.apply(self, args).then(function stopped() {
+				return forward.call(self, "stop", args);
 			});
 		},
-		"sig/finalize" : forward
+
+		"sig/finalize" : function onFinalize() {
+			return forward.call(this, "finalize", arguments);
+		}
 	});
 });
