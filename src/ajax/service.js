18d05f4f81170a09b95a61bc578a18b1d68cb1d7
Mikael Karon <mikael@karon.se>
2013-03-20 14:25:43 +0800

    Update ajax service to return array of arguments. see troopjs/troopjs-core#98

--- src/ajax/service.js
+++ src/ajax/service.js
@@ -3,17 +3,24 @@
  * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
  */
 /*global define:false */
-define([ "troopjs-core/component/service", "jquery", "troopjs-utils/merge" ], function AjaxModule(Service, $, merge) {
+define([ "troopjs-core/component/service", "jquery", "troopjs-utils/merge", "when" ], function AjaxModule(Service, $, merge, when) {
+	var ARRAY_SLICE = Array.prototype.slice;
+
 	return Service.extend({
 		"displayName" : "browser/ajax/service",
 
 		"hub/ajax" : function ajax(settings) {
 			// Request
-			return $.ajax(merge.call({
+			var request = $.ajax(merge.call({
 				"headers": {
 					"x-request-id": new Date().getTime()
 				}
 			}, settings));
+
+			// Wrap and return
+			return when(request, function () {
+				return ARRAY_SLICE.call(arguments);
+			});
 		}
 	});
 });
