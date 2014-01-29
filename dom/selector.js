279213d3ffa904e8256a206faa91c7824744ce04
Mikael Karon <mikael@karon.se>
2014-01-29 19:34:03 +0800

    Implement code for `add` and `matches` WIP

--- dom/selector.js
+++ dom/selector.js
@@ -1,9 +1,78 @@
-define([ "troopjs-core/object/factory" ], function (Factory) {
+define([ "troopjs-core/object/factory", "./constants", "./config" ], function (Factory, CONSTANTS, CONFIG) {
+	var UNDEFINED;
 	var LENGTH = "length";
+	var INDEXES = "indexes";
+	var INDEXED = "indexed";
+	var INDEXER = "indexer";
 	var SLASH = "\\";
 	var SPACE = " ";
+	var CLASS = "class";
+	var ID = "id";
+	var TAG = "tag";
+	var UNIVERSAL = "universal";
+	var RE_SPACE = /\s+/;
+	var querySelectorAll = CONFIG[CONSTANTS["querySelectorAll"]];
+	var matchesSelector = CONFIG[CONSTANTS["matchesSelector"]];
 
-	return Factory({
+	/**
+	 * Extracts key for universal indexer
+	 * @private
+	 * @return {String[]}
+	 */
+	function getElementUniversal() {
+		return [ "*" ];
+	}
+
+	/**
+	 * Extracts key for tag indexer
+	 * @private
+	 * @param element
+	 * @return {String[]}
+	 */
+	function getElementTagName(element) {
+		return [ element.nodeName.toUpperCase() ];
+	}
+
+	/**
+	 * Extracts key for class indexer
+	 * @private
+	 * @param element
+	 * @return {String[]}
+	 */
+	function getElementClassNames(element) {
+		var className;
+		var result;
+
+		// Do we have a `className` property
+		if ((className = element.className) !== UNDEFINED) {
+			// DOM `className`
+			if (typeof className === "string") {
+				result = className.split(RE_SPACE);
+			}
+			// SVG `className`
+			else if (className instanceof SVGAnimatedString) {
+				result = className.baseVal.split(RE_SPACE);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Extracts key for id indexer
+	 * @private
+	 * @param element
+	 * @return {String[]}
+	 */
+	function getElementId(element) {
+		var id;
+
+		return (id = element.id) !== UNDEFINED && [ id ];
+	}
+
+	return Factory(function Selector() {
+		this[INDEXES] = [];
+	}, {
 		/**
 		 * Gets last token from selector
 		 * @param {String} selector CSS selector
@@ -69,6 +138,118 @@ define([ "troopjs-core/object/factory" ], function (Factory) {
 			}
 
 			return selector.substring(index, stop);
+		},
+
+		/**
+		 * Adds candidate
+		 * @param {String} selector CSS selector
+		 * @param {...*} [args] Additional arguments attached with candidate
+		 * @return {Object} this
+		 */
+		"add": function add(selector, args) {
+			var me = this;
+			var indexes = me[INDEXES];
+			var indexed;
+			var indexer;
+			var index;
+			var name;
+			var key;
+			var last = me.last(selector);
+
+			switch (last[0]) {
+				case "#":
+					name = ID;
+					key = last.substring(1);
+					indexer = getElementId;
+					break;
+
+				case ".":
+					name = CLASS;
+					key = last.substring(1);
+					indexer = getElementClassNames;
+					break;
+
+				case "*":
+					name = UNIVERSAL;
+					key = "*";
+					indexer = getElementUniversal;
+					break;
+
+				default:
+					name = TAG;
+					key = last.toUpperCase();
+					indexer = getElementTagName;
+					break;
+			}
+
+			if ((index = indexes[name]) === UNDEFINED) {
+				index = indexes[name] = indexes[indexes[LENGTH]] = {};
+				index[INDEXER] = indexer;
+				indexed = index[INDEXED] = {};
+			}
+			else {
+				indexed = index[INDEXED];
+			}
+
+			if (key in indexed) {
+				indexed[key].push(arguments);
+			}
+			else {
+				indexed[key] = [ arguments ];
+			}
+
+			return me;
+		},
+
+		/**
+		 * Matches candidates against element
+		 * @param element DOM Element
+		 * @return {Array} Matching array of candidates
+		 */
+		"matches": function (element) {
+			var me = this;
+			var indexer;
+			var indexed;
+			var indexes = me[INDEXES];
+			var index;
+			var indexCount = indexes[LENGTH];
+			var keys;
+			var keysCount;
+			var candidates;
+			var candidate;
+			var candidateCount;
+			var result = [];
+			var resultCount = 0;
+
+			if (!element) {
+				return result;
+			}
+
+			while (indexCount--) {
+				index = indexes[indexCount];
+				indexer = index[INDEXER];
+				indexed = index[INDEXED];
+
+				keys = indexer(element);
+				keysCount = keys[LENGTH];
+
+				while (keysCount--) {
+
+					if ((candidates = indexed[keys[keysCount]]) !== UNDEFINED) {
+						candidateCount = candidates[LENGTH];
+
+						while (candidateCount--) {
+							candidate = candidates[candidateCount];
+
+							if (matchesSelector(element, candidate[0])) {
+								result[resultCount++] = candidate;
+							}
+						}
+					}
+				}
+			}
+
+			return result;
 		}
 	});
 });
--- test/dom/selector.js
+++ test/dom/selector.js
@@ -5,7 +5,7 @@ buster.testCase("troopjs-browser/dom/selector", function (run) {
 	var assert = buster.referee.assert;
 	var refute = buster.referee.refute;
 
-	require([ "troopjs-browser/dom/selector" ], function (Selector) {
+	require([ "troopjs-browser/dom/selector", "jquery" ], function (Selector, $) {
 		run({
 			".last able to extract tag/id/class": function () {
 				var selector = Selector();
@@ -59,6 +59,22 @@ buster.testCase("troopjs-browser/dom/selector", function (run) {
 				var selector = Selector();
 
 				assert.equals(selector.last(""), "");
+			},
+
+			"add": function () {
+				var selector = Selector();
+
+				selector.add("tag", 1);
+				selector.add("tag#id", 2);
+				selector.add("tag#id.class1", 3);
+				selector.add(".class1", 4);
+				selector.add(".class2", 5);
+				selector.add("#id", 6);
+				selector.add("*", 7);
+
+				console.log(selector.matches($("<tag id='id' class='class1 class2' />").get(0)));
+
+				assert(true);
 			}
 		});
 	});
