4cd3406d4c3b0aa87bfe4b535002e623623cc730
Mikael Karon <mikael@karon.se>
2014-11-26 17:31:54 +0800

    Fix delegated/direct events (once and for all?)

--- runner/sequence.js
+++ runner/sequence.js
@@ -17,12 +17,7 @@ define([
 	 */
 
 	var UNDEFINED;
-	var CONTEXT = "context";
-	var DATA = "data";
-	var HEAD = "head";
-	var NEXT = "next";
-	var SELECTOR = "selector";
-	var MODIFIED = "modified";
+	var LEFT_BUTTON = 0;
 
 	// Use `$.find.matchesSelector` for wider browser support
 	SelectorSet.prototype.matchesSelector = $.find.matchesSelector;
@@ -34,74 +29,58 @@ define([
 	 * @return {*} Result from last handler
 	 */
 	return function sequence(event, handlers, args) {
-		var modified = handlers[MODIFIED];
+		var $reduce = function (result, handler) {
+			// If immediate propagation is stopped we should just return last result
+			if ($event.isImmediatePropagationStopped()) {
+				return result;
+			}
+
+			// If the previous handler return false we should stopPropagation and preventDefault
+			if (result === false) {
+				$event.stopPropagation();
+				$event.preventDefault();
+			}
+
+			return handler.apply(handler.context, args);
+		};
+
 		var $event = args[0];
-		var selector;
-		var candidate;
-		var root = $event.delegateTarget;
-		var current = $event.target;
-		var elements;
-		var isClick;
+		var $delegate = $event.delegateTarget;
+		var $target = $event.target;
+		var $notClick = $event.type !== "click";
+		var $result = UNDEFINED;
+		var direct = handlers.direct;
+		var delegated = handlers.delegated;
+		// we bubble the event up the dom if:
+		// 1. this is not a black-holed element (jQuery #13180)
+		// 2. and: this is the left button or it's a not a click event (jQuery #3861)
+		var bubbleUp = $target.nodeType !== UNDEFINED && ($event.button === LEFT_BUTTON || $notClick);
 
-		// Try get SELECTOR from handlers and check if MODIFIED
-		if ((selector = handlers[SELECTOR]) === UNDEFINED || selector[MODIFIED] !== modified) {
-			// Create and cache SELECTOR
-			selector = handlers[SELECTOR] = new SelectorSet();
-
-			// Set MODIFIED on selector
-			selector[MODIFIED] = modified;
-
-			// Iterate handlers
-			for (candidate = handlers[HEAD]; candidate !== UNDEFINED; candidate = candidate[NEXT]) {
-				// Add candidate with selector or default selector '*'
-				selector.add(candidate[DATA] || "*", candidate);
+		do {
+			// Don't process clicks on disabled elements (jQuery #6911, #8165, #11382, #11764)
+			if ($target.disabled !== true || $notClick) {
+				// run delegated handlers which match this element
+				$result = delegated // selector set of delegated selector-handler pairs
+					.matches($event.currentTarget = $target)
+					.map(function(match){
+						// we only need the handler function
+						return match[1]; // match[0] is the selector. match[1] is the handler
+					})
+					.reduce($reduce, $result);
 			}
 		}
+		while (
+			bubbleUp &&
+			!$event.isPropagationStopped() &&
+			$target !== $delegate && // stop bubbling up at the root element
+			($target = $target.parentNode) !== null // bubble up until nowhere to go
+		);
 
-		// If the event target is the same as the delegateTarget we can just match against current ...
-		if (root === current) {
-			elements = current;
-		}
-		// ... otherwise we have to build a delegate tree of elements
-		//
-		// Black-hole SVG <use> instance trees (jQuery #13180)
-		// Avoid non-left-click bubbling in Firefox (jQuery #3861)
-		else if(current.nodeType !== UNDEFINED && ($event.button !== 0 || !(isClick = $event.type === "click"))) {
-			// Let `elements` be `[]`
-			elements = [];
-
-			do {
-				// Don't process clicks on disabled elements (jQuery #6911, #8165, #11382, #11764)
-				if (current.disabled !== true || !isClick) {
-					elements.push(current);
-				}
-			} while (current !== root && (current = current.parentNode) !== null);
-		}
-		else {
-			return UNDEFINED;
+		// run all the direct (non-delegated) handlers of the root element
+		if ($result !== false) {
+			$result = direct.reduce($reduce, $result);
 		}
 
-		return selector
-			// Filter to only selectors that match target
-			.matches(elements)
-			// Reduce so we can catch the end value
-			.reduce(function (result, selector) {
-				// If immediate propagation is stopped we should just return last result
-				if ($event.isImmediatePropagationStopped()) {
-					return result;
-				}
-
-				// If the previous candidate return false we should stopPropagation and preventDefault
-				if (result === false) {
-					$event.stopPropagation();
-					$event.preventDefault();
-				}
-
-				// Get candidate from selector
-				var candidate = selector[1];
-
-				// Run candidate, provide result to next run
-				return candidate.apply(candidate[CONTEXT], args);
-			}, UNDEFINED);
+		return $result;
 	}
 });
